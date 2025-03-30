5b954ed4fef39dde33c4a59cb40ff1e7eb222bda
gavinlai <laichikwan01@gmail.com>
2025-03-30 17:07:58 +0800

    Remove commented-out code and unnecessary dependencies from Chroma model

--- app/models/chroma/__init__.py
+++ app/models/chroma/__init__.py
@@ -1,27 +1,27 @@
-"""Module for the Vector Database."""
-from langchain_chroma import Chroma
-from models.llm import EmbeddingsModel
+# """Module for the Vector Database."""
+# from langchain_chroma import Chroma
+# from models.llm import EmbeddingsModel
 
-vectorstore = Chroma(
-    embedding_function=EmbeddingsModel("all-MiniLM-L6-v2"),
-    collection_name="email",
-    persist_directory="models/chroma/data"
-)
+# vectorstore = Chroma(
+#     embedding_function=EmbeddingsModel("all-MiniLM-L6-v2"),
+#     collection_name="email",
+#     persist_directory="models/chroma/data"
+# )
 
-# def create_or_get_collection(collection_name: str):
-#     """
-#     Creates a new collection or gets an existing collection from the Vector Database.
+# # def create_or_get_collection(collection_name: str):
+# #     """
+# #     Creates a new collection or gets an existing collection from the Vector Database.
 
-#     Args:
-#         collection_name (str): The name of the collection.
+# #     Args:
+# #         collection_name (str): The name of the collection.
 
-#     Returns:
-#         chromadb.Collection: The collection associated with the provided name.
-#     """
-#     chroma_client = chromadb.PersistentClient(path="models/chroma/data")
-#     collection = chroma_client.get_or_create_collection(collection_name)
-#     # try:
-#     #     collection = chroma_client.create_collection(collection_name)
-#     # except chromadb.errors.UniqueConstraintError:
-#     #     collection = chroma_client.get_collection(collection_name)
-#     return collection
+# #     Returns:
+# #         chromadb.Collection: The collection associated with the provided name.
+# #     """
+# #     chroma_client = chromadb.PersistentClient(path="models/chroma/data")
+# #     collection = chroma_client.get_or_create_collection(collection_name)
+# #     # try:
+# #     #     collection = chroma_client.create_collection(collection_name)
+# #     # except chromadb.errors.UniqueConstraintError:
+# #     #     collection = chroma_client.get_collection(collection_name)
+# #     return collection
