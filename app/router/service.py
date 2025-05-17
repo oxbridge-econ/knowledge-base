ddc0267e04dc3c9ce9257ab1661998c5dcb07a2e
gavinlai <laichikwan01@gmail.com>
2025-05-17 23:17:43 +0800

    Refactor Gmail routes to use EmailQuery model and improve credential handling

--- app/router/service.py
+++ app/router/service.py
@@ -2,17 +2,17 @@
 import os
 import threading
 from google.oauth2.credentials import Credentials
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from fastapi.responses import JSONResponse
 
 from services import GmailService
-from schema import MailReqData
+from schema import EmailQuery
 from models.db import MongodbClient
 
-router = APIRouter(prefix="/service", tags=["mail"])
+router = APIRouter(prefix="/service", tags=["service"])
 
-@router.post("/gmail")
-def collect(body: MailReqData) -> JSONResponse:
+@router.post("/gmail/collect")
+def collect(body: EmailQuery, email: str = Query(...)) -> JSONResponse:
     """
     Handles the chat POST request.
 
@@ -23,7 +23,7 @@ def collect(body: MailReqData) -> JSONResponse:
         str: The generated response from the chat function.
     """
     collection = MongodbClient["user"]["FinFAST"]
-    cred_dict = collection.find_one({"_id": body.email})
+    cred_dict = collection.find_one({"_id": email})
     if cred_dict is None:
         return JSONResponse(content={"error": "User not found."}, status_code=404)
     credentials = Credentials(
@@ -38,14 +38,12 @@ def collect(body: MailReqData) -> JSONResponse:
         return JSONResponse(content={"valid": False,
                                      "error": "Invalid or expired credentials."}, status_code=401)
     service = GmailService(credentials)
-    if body.query is not None:
-        threading.Thread(target=service.collect, args=[body.query]).start()
-        return JSONResponse(content={"message": "Mail collection in progress."})
-    return JSONResponse(content={"error": "Query is required."}, status_code=400)
+    threading.Thread(target=service.collect, args=[body]).start()
+    return JSONResponse(content={"message": "Mail collection in progress."})
 
 
-@router.get("/gmail")
-def get(body: MailReqData) -> JSONResponse:
+@router.post("/gmail/preview")
+def preview(body: EmailQuery, email: str = Query(...)) -> JSONResponse:
     """
     Handles the chat POST request.
 
@@ -56,7 +54,7 @@ def get(body: MailReqData) -> JSONResponse:
         str: The generated response from the chat function.
     """
     collection = MongodbClient["user"]["FinFAST"]
-    cred_dict = collection.find_one({"_id": body.email})
+    cred_dict = collection.find_one({"_id": email})
     if cred_dict is None:
         return JSONResponse(content={"error": "User not found."}, status_code=404)
     credentials = Credentials(
@@ -70,7 +68,31 @@ def get(body: MailReqData) -> JSONResponse:
     if not credentials.valid or credentials.expired:
         return JSONResponse(content={"valid": False}, status_code=401)
     service = GmailService(credentials)
-    if body.query is not None:
-        print(body.query)
-        return JSONResponse(content=service.get(body.query))
+    return JSONResponse(content=service.get(body))
+
+@router.get("/gmail")
+def valid(email: str = Query(...)) -> JSONResponse:
+    """
+    Handles the chat POST request.
+
+    Args:
+        query (ReqData): The request data containing the query parameters.
+
+    Returns:
+        str: The generated response from the chat function.
+    """
+    collection = MongodbClient["user"]["FinFAST"]
+    cred_dict = collection.find_one({"_id": email})
+    if cred_dict is None:
+        return JSONResponse(content={"error": "User not found."}, status_code=404)
+    credentials = Credentials(
+        token=cred_dict["token"],
+        refresh_token=cred_dict["refresh_token"],
+        token_uri="https://oauth2.googleapis.com/token",
+        client_id=os.environ.get("CLIENT_ID"),
+        client_secret=os.environ.get("CLIENT_SECRET"),
+        scopes=["https://www.googleapis.com/auth/gmail.readonly"],
+    )
+    if not credentials.valid or credentials.expired:
+        return JSONResponse(content={"valid": False}, status_code=401)
     return JSONResponse(content={"valid": True})
