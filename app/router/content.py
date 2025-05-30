97771338588311b3973f3e8a1fcd65206d88156d
gavinlai <laichikwan01@gmail.com>
2025-05-30 14:46:43 +0800

    Comment out the stream route and its associated docstring in content.py

--- app/router/content.py
+++ app/router/content.py
@@ -6,15 +6,15 @@ from utils import generate
 
 router = APIRouter(tags=["content"])
 
-@router.post("/stream")
-async def stream(query: ReqData):
-    """
-    Handles streaming of data based on the provided query.
+# @router.post("/stream")
+# async def stream(query: ReqData):
+#     """
+#     Handles streaming of data based on the provided query.
 
-    Args:
-        query (ReqData): The request data containing the query parameters.
+#     Args:
+#         query (ReqData): The request data containing the query parameters.
 
-    Returns:
-        StreamingResponse: A streaming response with generated data with type 'text/event-stream'.
-    """
-    return StreamingResponse(generate(query), media_type='text/event-stream')
+#     Returns:
+#         StreamingResponse: A streaming response with generated data with type 'text/event-stream'.
+#     """
+#     return StreamingResponse(generate(query), media_type='text/event-stream')
