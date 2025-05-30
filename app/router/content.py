1426fea5bd6b7116beabf8c31eb4275da5adcded
gavinlai <laichikwan01@gmail.com>
2025-05-30 15:07:38 +0800

    Remove commented-out stream route and associated docstring from content.py

--- app/router/content.py
+++ app/router/content.py
@@ -1,20 +1,4 @@
 """Module for defining the main routes of the API."""
 from fastapi import APIRouter
-from fastapi.responses import StreamingResponse
-from schema import ReqData
-from utils import generate
 
 router = APIRouter(tags=["content"])
-
-# @router.post("/stream")
-# async def stream(query: ReqData):
-#     """
-#     Handles streaming of data based on the provided query.
-
-#     Args:
-#         query (ReqData): The request data containing the query parameters.
-
-#     Returns:
-#         StreamingResponse: A streaming response with generated data with type 'text/event-stream'.
-#     """
-#     return StreamingResponse(generate(query), media_type='text/event-stream')
