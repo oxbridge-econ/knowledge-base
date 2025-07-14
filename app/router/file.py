9193de8be2b1413a488bf650978d5a4fb0a4ce32
MusaddiqAl <mali@oxbridge-econ.com>
2025-07-14 15:51:52 +0800

    feat: check if the file with the same as being uploaded is already stored

--- app/router/file.py
+++ app/router/file.py
@@ -4,7 +4,7 @@ import threading
 from fastapi import APIRouter, File, UploadFile, HTTPException, Query
 from fastapi.responses import JSONResponse
 from controllers.utils import upsert
-from controllers.loader import load_docx, load_pdf, load_img
+from controllers.loader import load_docx, load_pdf, load_img, FileAlreadyExistsError,  upload_file_to_azure
 from schema import task_states
 
 router = APIRouter(prefix="/file", tags=["file"])
@@ -37,22 +37,33 @@ async def load(file: UploadFile = File(...), email: str = Query(...)) -> JSONRes
     }
     task_states[task["id"]] = "Pending"
     upsert(email, task)
-    if file.content_type == "application/pdf":
+    try:
+        #check if file already exists
+        upload_file_to_azure(content, file.filename, email, file.content_type)
 
-        threading.Thread(target=load_pdf, args=(content, file.filename, email, task, file.content_type)).start()
-    elif file.content_type == \
-        "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-        threading.Thread(target=load_docx, args=(content, file.filename, email, task, file.content_type)).start()
-    elif file.content_type in ["image/png", "image/jpeg"]:
-        threading.Thread(target=load_img, args=(content, file.filename, email, task, file.content_type)).start()
-    else:
+        if file.content_type == "application/pdf":
+            threading.Thread(target=load_pdf, args=(content, file.filename, email, task, file.content_type)).start()
+        elif file.content_type == \
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            threading.Thread(target=load_docx, args=(content, file.filename, email, task, file.content_type)).start()
+        elif file.content_type in ["image/png", "image/jpeg"]:
+            threading.Thread(target=load_img, args=(content, file.filename, email, task, file.content_type)).start()
+        else:
+            task["status"] = "failed"
+            task_states[task["id"]] = "Failed"
+            upsert(email, task)
+            raise HTTPException(
+                status_code=400,
+                detail="Invalid file type. Only PDF, TXT, and DOCX are allowed."
+            )
+    except FileAlreadyExistsError as e:
         task["status"] = "failed"
         task_states[task["id"]] = "Failed"
         upsert(email, task)
         raise HTTPException(
-            status_code=400,
-            detail="Invalid file type. Only PDF, TXT, and DOCX are allowed."
-        )
+            status_code=409,  # 409 Conflict status code for duplicate resource
+            detail=str(e)
+        ) from e
 
     return JSONResponse(content=task)
 
