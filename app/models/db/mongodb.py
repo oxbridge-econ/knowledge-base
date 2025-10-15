b8397ce9e3c9a883f9bb38729a67e5cf835e77b5
OxbridgeEconomics <115981404+oxbridge-economics@users.noreply.github.com>
2025-10-15 20:05:27 +0800

    Update mongodb.py

--- app/models/db/mongodb.py
+++ app/models/db/mongodb.py
@@ -5,4 +5,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MongodbClient = MongoClient(os.getenv('MONGODB_URL'))
+MongodbClient = MongoClient(os.getenv('MONGODB_URL')) # pylint: disable=invalid-name
